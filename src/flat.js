db835913cbf8d365080d3ec6c4e19d57657e289a
felipebattistotti <feligatti@gmail.com>
2020-03-04 23:36:57 -0300

    Commits novos .js

---
+++ src/flat.js
@@ -0,0 +1,5 @@
+let array1 = ['a','b', [1, 2, 3]];
+let array2 = array1.flat();
+
+console.log(array2);
+// ["a", "b", 1, 2, 3]
---
+++ src/flatMap.js
@@ -0,0 +1,5 @@
+let array1 = [1, 2, 3, 4];
+array1 = array1.flatMap(x => [x + 1]);
+
+console.log(array1);
+// [2, 3, 4, 5]
